6b8754820f20acb5d171348b7016b2dc62846f30
Peyton <peytonsemail2000@gmail.com>
2022-02-18 10:37:32 -0600

    Commented main

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -3,29 +3,34 @@
 
 void main()
 {
+	//Creates two new lists
 	List<int> list = List<int>();
 	List<int> listTwo;
 
-	list.sort();
+	//Tests adding several items to list in different ways and sorts them
 	list.pushBack(10);
 	list.pushFront(21);
 	list.insert(20, 1);
 	list.insert(50, -1);
 	list.insert(40, 5);
 	list.sort();
-	list.initialize();
+
+	//Inserts into list two, but then sets list two equal to list one
 	listTwo.insert(60, 1);
 	listTwo.sort();
 	listTwo = list;
 
+	//For the length of the list
 	for (Iterator<int> iter = list.begin(); iter != list.end(); ++iter)
 	{
+		//If it contains 10, print success
 		if (*iter == 10)
 		{
 			std::cout << "success" << std::endl;
 		}
 	}
 
+	//Print the two lists
 	listTwo.print();
 	list.print();
 
